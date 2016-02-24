5292037cf36b49e06115b4d7fa737fd4bc8a893a
ROBEL TEKESTE <ROBEL.TEKESTE@GMAIL.COM>
2016-02-23 21:21:12 -0500

    testing

---
+++ pseudo.md
@@ -0,0 +1,158 @@
+Project 1: The Big Bang Theory Trivia Game
+
+Trivia Questions:
+  1. What change sets Sheldon off the moment that he enters the apartment after returning from his "life on the rails"?
+    A. Penny's new haircut?
+    B. Raj's new GF being there?
+    C. The furniture has been rearranged
+    D. Bernadette sitting in his 'spot'
+    (Answer) A
+
+  2. Finish this game title: "Rock, paper, scissors, lizard, ______"
+      A. Krk
+      B. Piccard
+      C. Spock
+      D. Data
+      (Answer) C
+
+  3. What is Raj's middle name?
+      A. Naveen
+      B. Amar
+      C. Ramayan
+      D. Neil
+      (Answer) C
+
+  4. Why does Sheldon sing "Soft Kitty" to Penny in "The Adhesive Duck Deficiency"?
+      A. Because she has the flu?
+      B. Because she dislocates her shoulder?
+      C. Because she's homesick?
+      D. Because she misses Leonard?
+      (Answer) B
+
+  5. Which articles of clothing did Penny's ex-boyfriend take from Leonard and Sheldon when they try to retrieve her TV?
+      A. Their shirts?
+      B. Their pants?
+      C. Their underwear?
+      D. Their shoes?
+      (Answer) B
+
+  6. What mistake does Howard make when he tries to impress Dr. Stephanie Barnett?
+      A. He gets the Mars rover stuck in a ditch?
+      B. He introduces her to his mother?
+      C. He accidentally eats peanuts?
+      D. He crashes his scooter?
+      (Answer) A
+
+  7. What major secret about Penny is revealed in "The Thanksgiving Decoupling"?
+      A. That she's moving back to Nebraska?
+      B. That she's breaking up with Leonard?
+      C. That she's going to be in a movie with Jennifer Anniston?
+      D. That she married Zack in Vegas 3 years ago?
+      (Answer) D
+
+  8. What are Penny, Amy, Sheldon, and Leonard in the middle of when Howard returns from space that causes them to yell, "NOT NOW!" at him?
+      A. Watching a horror movie?
+      B. A pie eating contest?
+      C. Packing for a camping trip?
+      D. The finale of 'Lost'?
+      (Answer) B
+
+  9. What does Sheldon do when he meets Stephen Hawking and Dr. Hawking points out a mathematical error of Sheldon's?
+      A. He tells Dr. Hawking that he's wrong?
+      B. He faints?
+      C. He puts Dr. Hawking on his mortal enemy list?
+      D. He cries?
+      (Answer) B
+
+  10. What is Raj's signature drink?
+      A. Strawberry Margaritas?
+      B. Moscow mules?
+      C. Grasshoppers?
+      D. Pina Coladas?
+      (Answer) C
+
+  11. Who lends Penny money when she can't afford to pay her rent in "The Financial Permeability"?
+    A. Amy?
+    B. Sheldon?
+    C. Howard?
+    D. Leonard?
+    (Answer) B
+
+  12. How does Penny accidentally ruin Sheldon's favorite couch spot in "The Cushion Saturation"?
+    A. She rips the cushion with a nail file?
+    B. She throws up on the cushion after drinking too much?
+    C. She shoots the cushion with a paintball gun?
+    D. She spills nail polish on the cushion?
+    (Answer) C
+
+  13. What does Sheldon use as bait to positively reinforce better habits out of Penny?
+    A. Wine?
+    B. Potato Chips?
+    C. Chocolate?
+    D. Money?
+    (Answer) C
+
+  14. What is the name of Penny's business portrayed in "The Work Song Nano-cluster"?
+    A. Penny Blossoms?
+    B. Penny's Treasures?
+    C. Penny's Pretties?
+    D. Designs by Penny?
+    (Answer) A
+
+  15. What is Stuart's self-proclaimed "dream job" that he lands after the comic store burns down due to a "hot plate incident"?
+    A. Mrs. Wolowitz's caregiver?
+    B. Grooming Raj's dog, Cinnamon, full-time?
+    C. Running his rival's comic book store?
+    D. Dressing up as "Goofy" at Disneyland?
+    (Answer) A
+
+  16. Where do Howard and Bernadette get married before Howard goes into space?
+    A. A catholic church in Pasadena?
+    B. City Hall?
+    C. Vegas?
+    D. The roof of the guys' apartment building?
+    (Answer) D
+
+  17. What is Howard Allergic to?
+    A. Shellfish?
+    B. Strawberries?
+    C. Nuts
+    D. Eggs?
+    (Answer) C
+
+  18. Which superhero moniker does Leonard frequently use as his password?
+    A. Kal-El?
+    B. Bruce Wayne?
+    C. Peter Parker?
+    D. Hal Jordan?
+    (Answer) A
+
+  19. Which of the following embarrassing things did Howard do on his blind date with Emily, Raj's girlfriend?
+    A. Forget his wallet?
+    B. Call her by the wrong name all night?
+    C. Sneeze when he tried to kiss her?
+    D. Clog her apartment toilet?
+    (Answer) D
+
+  20. In Season 3, after Penny and Leonard break up, what dinner does Penny make Sheldon that reminds him of his mother?
+    A. Grilled Cheese and Tomato soup?
+    B. Spaghettios?
+    C. Mac 'N Cheese?
+    D. Spaghetti with cut up hot dogs?
+    (Answer) D
+
+  21. Who said it: "From the waist down, my shields are up."
+    A. Sheldon
+    B. Wolowitz
+    C. Penny
+    D. Bernadette
+    (Answer) C
+
+
+//Gold Version
+
+
+//Silver Version
+
+
+//Bronze Version
---
+++ script.js
@@ -0,0 +1,171 @@
+(function() {
+  var questions = [{
+    question: "What change sets Sheldon off the moment that he enters the apartment after returning from his 'life on the rails'?",
+    choices: ["Penny's new haircut", "Raj's new girlfriend being there", "The furniture has been rearranged", "Bernadette sitting in his spot"],
+    correctAnswer: 0
+  },
+  {
+    question: "Finish this game title: 'Rock, paper, scissors, lizard, ______'",
+    choices: ["Kirk", "Piccard", "Spock", "Data"],
+    correctAnswer: 2
+  },
+  {
+    question: "What is Raj's middle name?",
+    choices: ["Naveen", "Amar", "Ramayan", "Neil"],
+    correctAnswer: 2
+  },
+  {
+    question: "Why does Sheldon sing 'Soft Kitty' to Penny in 'The Adhesive Duck Deficiency'?",
+    choices: ["Because she has the flu", "Because she dislocates her shoulder", "Because she's homesick", "Because she misses Leonard"],
+    correctAnswer: 1
+  },
+  {
+    question: "Which articles of clothing did Penny's ex-boyfriend take from Leonard and Sheldon when they try to retrieve her TV?",
+    choices: ["Their shirts", "Their pants", "Their underwear", "Their shoes"],
+    correctAnswer: 1
+  }];
+
+  var questionCounter = 0; //Tracks question number
+  var selections = []; //Array containing user choices
+  var quiz = $('#quiz'); //Quiz div object
+
+  // Display initial question
+  displayNext();
+
+  // Click handler for the 'next' button
+  $('#next').on('click', function (e) {
+    e.preventDefault();
+
+    // Suspend click listener during fade animation
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+
+    // If no user selection, progress is stopped
+    if (isNaN(selections[questionCounter])) {
+      alert('Please make a selection!');
+    } else {
+      questionCounter++;
+      displayNext();
+    }
+  });
+
+  // Click handler for the 'prev' button
+  $('#prev').on('click', function (e) {
+    e.preventDefault();
+
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+    questionCounter--;
+    displayNext();
+  });
+
+  // Click handler for the 'Start Over' button
+  $('#start').on('click', function (e) {
+    e.preventDefault();
+
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+  });
+
+  // Animates buttons on hover
+  $('.button').on('mouseenter', function () {
+    $(this).addClass('active');
+  });
+  $('.button').on('mouseleave', function () {
+    $(this).removeClass('active');
+  });
+
+  // Creates and returns the div that contains the questions and
+  // the answer selections
+  function createQuestionElement(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+
+    return qElement;
+  }
+
+  // Creates a list of the answer choices as radio inputs
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+
+  // Reads the user selection and pushes the value to an array
+  function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  }
+
+  // Displays next requested element
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+
+      if(questionCounter < questions.length){
+        var nextQuestion = createQuestionElement(questionCounter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[questionCounter]))) {
+          $('input[value='+selections[questionCounter]+']').prop('checked', true);
+        }
+
+        // Controls display of 'prev' button
+        if(questionCounter === 1){
+          $('#prev').show();
+        } else if(questionCounter === 0){
+
+          $('#prev').hide();
+          $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+        $('#prev').hide();
+        $('#start').show();
+      }
+    });
+  }
+
+  // Computes score and returns a paragraph element to be displayed
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+
+    var numCorrect = 0;
+    for (var i = 0; i < selections.length; i++) {
+      if (selections[i] === questions[i].correctAnswer) {
+        numCorrect++;
+      }
+    }
+
+    score.append('You got ' + numCorrect + ' questions out of ' +
+                 questions.length + ' right!!!');
+    return score;
+  }
+})();
