d2a750adb2f4a3baf7b4cd03e005b37e9ca9b670
ROBEL TEKESTE <ROBEL.TEKESTE@GMAIL.COM>
2016-02-24 22:03:04 -0500

    Second Commit

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var questions = [
   ["What change sets Sheldon off the moment that he enters the apartment after returning from his 'life on the rails'?","Penny's new haircut","Raj's new girlfriend being there","The furniture has been rearranged","Bernadette sitting in his spot","A"],
   ["Finish this game title: 'Rock, paper, scissors, lizard, ______'", "Kirk", "Piccard", "Spock", "Data", "C"],
   ["What is Raj's middle name?", "Naveen", "Amar", "Ramayan", "Neil", "C"],
-  ["Why does Sheldon sing 'Soft Kitty' to Penny in 'The Adhesive Duck Deficiency'?", "Because she has the flu?", "Because she dislocates her shoulder?", "Because she's homesick?", "Because she misses Leonard?", "B"],
+  ["Why does Sheldon sing 'Soft Kitty' to Penny in 'The Adhesive Duck Deficiency'?", "Because she has the flu", "Because she dislocates her shoulder", "Because she's homesick", "Because she misses Leonard", "B"],
   ["Which articles of clothing did Penny's ex-boyfriend take from Leonard and Sheldon when they try to retrieve her TV?", "Their shirts", "Their pants", "Their shoes",
   "Their underwear", "B"],
-  ["What mistake does Howard make when he tries to impress Dr. Stephanie Barnett?", "He gets the Mars rover stuck in a ditch?", "He introduces her to his mother?", "He accidentally eats peanuts?", "He crashes his scooter?", "A"],
+  ["What mistake does Howard make when he tries to impress Dr. Stephanie Barnett", "He gets the Mars rover stuck in a ditch", "He introduces her to his mother", "He accidentally eats peanuts", "He crashes his scooter", "A"],
   ["What major secret about Penny is revealed in 'The Thanksgiving Decoupling'?","That she's moving back to Nebraska","That she's breaking up with Leonard","That she's going to be in a movie with Jennifer Anniston","That she married Zack in Vegas 3 years ago","D"],
   ["What are Penny, Amy, Sheldon, and Leonard in the middle of when Howard returns from space that causes them to yell, 'NOT NOW!' at him?", "Watching a horror movie", "A pie eating contest", "Packing for a camping trip", "The finale of 'Lost'", "B"],
   ["What does Sheldon do when he meets Stephen Hawking and Dr. Hawking points out a mathematical error of Sheldon's?", "He tells Dr. Hawking that he's wrong", "He faints", "He puts Dr. Hawking on his mortal enemy list", "He cries", "B"],
@@ -39,7 +39,7 @@ function renderQuestion () {
   test.innerHTML += "<button onclick='checkAnswer()'>Submit</button>";
 }
 function checkAnswer(){
-  choices = document.getElementByName("choices");
+  choices = document.getElementsByName("choices");
     for (var i = 0; i<choices.length; i++) {
       if (choices[i].checked) {
         choice = choices[i].value;
